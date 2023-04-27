508cc262ce0da7ea2b3c6aca332975bf55c56136
AyimWilliam <odhisayim@gmail.com>
2023-04-27 10:25:32 +0300

    "finished excercie"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleFirstNumChange = (e) => {
+    setFirstNum(parseInt(e.target.value));
+  };
+
+  const handleSecondNumChange = (e) => {
+    setSecondNum(parseInt(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSum(firstNum + secondNum);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ADDING TWO NUMBERS</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Number 1:
+          <input type="number" value={firstNum} onChange={handleFirstNumChange} />
+        </label>
+        <br />
+        <label>
+          Number 2:
+          <input type="number" value={secondNum} onChange={handleSecondNumChange} />
+        </label>
+        <br />
+        <button type="submit">Add Your Two Numbers</button>
+      </form>
+      <p>The sum is: {sum}</p>
     </div>
-  );
+  )
 }
 
 export default App;
